814597cb5cc4dbfea87d742cf1c0fed5bdfeacc2
Avi Afrid <60225301+aviafrid781@users.noreply.github.com>
2022-11-09 10:20:48 +0600

    Update restaurant.interface.ts
    error fixed

--- src/restaurant/interfaces/restaurant.interface.ts
+++ src/restaurant/interfaces/restaurant.interface.ts
@@ -3,6 +3,5 @@ export interface RestaurantI extends Document {
   readonly name: string;
   readonly email: string;
   readonly address: string;
-  //readonly userId: string;
   readonly owner: string;
 }
